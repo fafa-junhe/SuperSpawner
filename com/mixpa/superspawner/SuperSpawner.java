b6d5efdfc1983e5eb28ed919aac19f18af33a3f2
fafa_junhe <842355358@qq.com>
2018-03-01 00:35:48 +0800

    Update SuperSpawner.java

--- com/mixpa/superspawner/SuperSpawner.java
+++ com/mixpa/superspawner/SuperSpawner.java
@@ -1,6 +1,11 @@
 package com.github.fafa;
 
 import org.bukkit.plugins.java.JavaPlugin;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+
+
 //服务器加载
 public class SuperSpawner extends JavaPlugins{
     public void onLoad(){
@@ -9,6 +14,9 @@ public class SuperSpawner extends JavaPlugins{
 //服务器关闭
     public void onDisable(){
         getLogger().info("[SuperSpawner]插件卸载完成")
+    }
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+        sender.sendMessage("插件已重载!")
     }
 }
      
